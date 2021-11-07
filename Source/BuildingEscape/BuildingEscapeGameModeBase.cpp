6b92c992b4f44c03adaf0690a2f1eedbfa97192c
lubmir2k <lubomir.mitana@protonmail.com>
2021-11-07 13:51:30 +0100

    Initial commit

---
+++ Source/BuildingEscape/BuildingEscapeGameModeBase.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "BuildingEscapeGameModeBase.h"
+
+// Pointers are memory address
+// You recognize them when you see * next to a type
+// Benefit is that they save having to copy/move data in memory
+// You can point it to any object
